48ed39d9d2d3583924cfc9a9521fc68578af68b4
Scripter <jtiks06@gmail.com>
2019-05-02 13:37:35 +0300

    do some work with 02.05.19

--- Labs/lab3.py
+++ Labs/lab3.py
@@ -2,38 +2,61 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def binary_search(f: 'function', a: float, b: float, eps: float = 1e-3) -> float:
+	if b < a:
+		raise ValueError("End of interval cannot be less than being")
+
+	if f(b)*f(a) > 0:
+		raise Exception("There is no root in passed interval.")
+
+	iteration_count = 0
+
+	middle = (a+b)/2
 	while b - a >= eps:
 		middle = (a + b)/2
+		iteration_count += 1
+		print("xl = {}, xx = {}, xp = {}".format(a, middle, b))
 		if f(middle) * f(a) < 0:
 			b = middle
 		if f(middle) * f(b) < 0:
 			a = middle
 		if f(middle) == 0:
 			return middle
+	print('Binary search number of iterations is:',iteration_count)
 	return middle
 
 
-def simple_iter(f : 'function', x:float, eps: float = 1e-5) -> float:
-	def fi(x: float) -> float:
-		return x + f(x)
-	while abs( x - fi(x)) >= eps:
-		x = fi(x)
-		print(x)
-		print('len=', abs( x - fi(x)))
+def simple_iter(x:float, eps: float) -> float:
+	iteration_count = 0
+	while True:
+		x_new = fi(x)
+		#print(x)
+		#print('len=', abs(x_new - x))
+
+		if abs(x_new - x) >= eps:
+			break
+		iteration_count += 1
+		#print(x)
+		x = x_new
+	print('Simple iteration method number of iteration', iteration_count)
 	return x
 
 
 def f(x: float) -> float:
 	return np.sin(2*x) - x**2
 
+def ro(x: float) -> float:
+	return 0.01
 
-def f_der(x: float) -> float:
-	return 2*np.cos(2*x) - 2*x 
+def fi(x: float) -> float:
+	return x + ro(x)*f(x)
 
 
-def f_der_second(x: float) -> float:
-	return -4*np.sin(2*x) - 2
+#def f_der(x: float) -> float:
+#	return 2*np.cos(2*x) - 2*x 
 
+
+#def f_der_second(x: float) -> float:
+#	return -4*np.sin(2*x) - 2
 
 
 def newton(f: 'function', f_der: 'function', x: float, eps: float = 1e-5) -> float:
@@ -54,6 +77,13 @@ def proof(f, f_der, f_der_second, a: float, b: float, n: int):
 	plt.plot(x, y)
 	plt.show()
 
+def proof_simple_iter(a, b, n = 100):
+	x = np.linspace(a, b, n)
+	y = np.array([f_der(x_i) for x_i in x])
+	y *= ro(x)
+	y += 1
+	plt.plot(x, y)
+	plt.show()
 
 def plot_f(f: 'function', a:float, b:float, n: int) -> float:
 	x = np.linspace(a, b, n)
@@ -64,8 +94,17 @@ def plot_f(f: 'function', a:float, b:float, n: int) -> float:
 
 
 if __name__ == '__main__':
-	a, b = -0.01, 0.02
-	x0 = binary_search(f, a, b)
-	print(newton(f, f_der, x0))
-	#plot_f(f, a, b, 100)
-	proof(f, f_der, f_der_second, a, b, 5)
+	#a, b = (float(x) for x in input("Input begin and end of interval: ").split())
+	#print("Your input is a={}, b={}".format(a, b))
+	#eps = float(input("Input epsilon: "))
+	#print("Your input is eps={}".format(eps))
+	a, b, eps, eps2 = 0.1, 0.8, 1e-3, 1e-5
+	plot_f(f, a, b, 100)
+	x0 = binary_search(f, a, b, eps)
+	print("Binary search result: ", x0)
+	#eps = float(input('Input epsilon for simple iteration: '))
+	#proof_simple_iter(a, b)
+	x1 = simple_iter(x0, eps2)
+	print('Simple iteration result =', x1)
+	#print(newton(f, f_der, x0))
+	#proof(f, f_der, f_der_second, a, b, 5)
