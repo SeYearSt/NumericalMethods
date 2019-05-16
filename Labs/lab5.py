13894ba9837fa09597352df5cdcc4bd4e576ccf2
Scripter <jtiks06@gmail.com>
2019-05-16 13:42:31 +0300

    Last version

--- Labs/lab5.py
+++ Labs/lab5.py
@@ -2,11 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import texttable as tt
 
-X = np.arange(0, 1, 0.1)
-Y = np.array([0., 0.22140, 0.49182, 0.82211, 1.22554, 1.71828, 2.32011, 3.05519, 3.95303, 5.04964])
-
-X_test = np.array([0.221, 0.428, 0.681, 0.3])
-
 
 def divided_diff(x: np.ndarray, y: np.ndarray) -> float:
     if y.shape != y.shape:
@@ -21,12 +16,12 @@ def divided_diff(x: np.ndarray, y: np.ndarray) -> float:
     return result
 
 
-def L(x: float) -> float:
-    result = Y[0]
-    for i in range(1, Y.shape[0]):
+def L(x: float, x_i: np.ndarray, y_i: np.ndarray) -> float:
+    result = y_i[0]
+    for i in range(1, y_i.shape[0]):
         indexes = np.arange(i)
         indexes_diff = np.append(indexes, indexes[-1]+1)
-        result += np.prod(x - X[indexes])*divided_diff(X[indexes_diff], Y[indexes_diff])
+        result += np.prod(x - x_i[indexes])*divided_diff(x_i[indexes_diff], y_i[indexes_diff])
 
     return result
 
@@ -36,7 +31,7 @@ def print_divided_diff_table(x: np.ndarray, y: np.ndarray) -> None:
 
     # ----------- set headers
     headings = ['X', 'Y']
-    for i in range(1, Y.shape[0]):
+    for i in range(1, y.shape[0]):
         head = 'f_x_i...f_x_i+{}'.format(i)
         headings.append(head)
 
@@ -46,9 +41,9 @@ def print_divided_diff_table(x: np.ndarray, y: np.ndarray) -> None:
 
     # --------- cal divided differences
 
-    for i in range(1, Y.shape[0]):
+    for i in range(1, y.shape[0]):
         divided_diff_s = np.array([divided_diff(x[j: j+i+1], y[j: j+i+1]) for j in range(y.shape[0] - i)])
-        divided_diff_s = np.hstack([divided_diff_s, np.zeros(Y.shape[0]-1-divided_diff_s.shape[0])])
+        divided_diff_s = np.hstack([divided_diff_s, np.zeros(y.shape[0]-1-divided_diff_s.shape[0])])
         values.append(divided_diff_s)
 
     for row in zip(*values):
@@ -59,6 +54,11 @@ def print_divided_diff_table(x: np.ndarray, y: np.ndarray) -> None:
 
 
 if __name__ == '__main__':
+
+    X = np.arange(0, 1, 0.1)
+    Y = np.array([0., 0.22140, 0.49182, 0.82211, 1.22554, 1.71828, 2.32011, 3.05519, 3.95303, 5.04964])
+
+    X_test = np.array([0.221, 0.428, 0.681, 0.3])
 
     for idx, node in enumerate(X):
         print("Value in table: {}, value of interpolation polygon: {}".format(Y[idx], L(node)))
@@ -78,7 +78,7 @@ if __name__ == '__main__':
         fig = plt.figure()
         plt.plot(X, Y, "ob", markersize=5)
         X_interp = np.linspace(X[0], X[-1], 100)
-        plt.plot(X_interp, [L(x) for x in X_interp], 'oy', markersize=2)
+        plt.plot(X_interp, [L(x, X, Y) for x in X_interp], 'oy', markersize=2)
         plt.legend(["Table point", "Interpolation"])
         plt.show()
 
